5e41571597437d9e36ed324009f2aaa336790686
igee-17 <igbagboleye@gmail.com>
2023-06-07 22:11:27 +0100

    home page section one

--- src/components/Home/SectionOne.js
+++ src/components/Home/SectionOne.js
@@ -1,7 +1,19 @@
 import React from "react";
+import darkwoman from "../../assets/images/darkwoman.png";
 
 const SectionOne = () => {
-  return <div>SectionOne</div>;
+  return (
+    <div
+      className="section-one"
+      style={{ backgroundImage: `url(${darkwoman})` }}
+    >
+      <div className="content">
+        <h1>Nigeria’s best financial and investment company.</h1>
+        <p>We help you save, invest and plan for generational wealth</p>
+        <button>Learn More</button>
+      </div>
+    </div>
+  );
 };
 
 export default SectionOne;
