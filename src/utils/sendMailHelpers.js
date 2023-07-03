28aa96bfcf753c1df32912f40b6772f4e4879d5e
igee-17 <igbagboleye@gmail.com>
2023-07-03 10:09:42 +0100

    http changed to https in endpoint

--- src/utils/sendMailHelpers.js
+++ src/utils/sendMailHelpers.js
@@ -11,14 +11,17 @@ export const contact = ({
   if (name && email && message && subject) {
     axios
       // .post("http://localhost:8080/attijara/contact", {
-      .post(" http://sagebackend-207c01373df4.herokuapp.com/attijara/contact", {
-        name,
-        email,
-        subject,
-        message,
-        fields,
-        recipient_email,
-      })
+      .post(
+        " https://sagebackend-207c01373df4.herokuapp.com/attijara/contact",
+        {
+          name,
+          email,
+          subject,
+          message,
+          fields,
+          recipient_email,
+        }
+      )
       .then(() => alert("Message Sent successfully"))
       .catch((err) => {
         console.log(err);
